407ec453fc57f6064691e6ad15ca46d84d1cbe3d
Polybunny <andrewberlinghoff@gmail.com>
2026-03-11 09:17:38 -0400

    01: created project, and created database

---
+++ database.py
@@ -0,0 +1,25 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# Connection string that points to SQLite file
+DATABASE_URL = "swlite:///./recipes.db"
+
+# Tell SQLite to allow FastAPI to run on multiple threads
+engine= create_engine(
+    DATABASE_URL,connect_args={"check_same_thread": False}
+)
+
+# autocommit=False -> changes won't save until explicitly committed
+# autoflush=False -> SQLAlchemy won't auto-push changes mid-session
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Base is class database models inherit from
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
---
+++ main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from database import Base, engine
+from sqlalchemy.orm import Session
+
+# Reads all models that inherit from Base -> creates corresponding tables in database file if they don't exist yet
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+
+def main():
+    print("Hello from recipemanager!")
+
+
+if __name__ == "__main__":
+    main()
